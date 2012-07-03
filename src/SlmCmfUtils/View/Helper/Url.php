f10031ec0fc71548c21c6bf885c25e941e052d56
Jurian Sluiman <jurian@soflomo.com>
2012-07-03 18:34:42 +0200

    Change license to 3-clause "New BSD"

--- src/SlmCmfUtils/View/Helper/Url.php
+++ src/SlmCmfUtils/View/Helper/Url.php
@@ -1,34 +1,42 @@
 <?php
-
-/*
- * This is free and unencumbered software released into the public domain.
- * 
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- * 
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- * 
- * For more information, please refer to <http://unlicense.org/>
- * 
- * @package    SlmCmfBase
- * @copyright  Copyright (c) 2009-2012 Soflomo (http://www.soflomo.com)
- * @license    http://unlicense.org Unlicense
+/**
+ * Copyright (c) 2012 Soflomo http://soflomo.com.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package     Ensemble\Utils
+ * @author      Jurian Sluiman <jurian@soflomo.com>
+ * @copyright   2012 Soflomo http://soflomo.com.
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link        http://ensemble.github.com
  */
 
 namespace SlmCmfUtils\View\Helper;
@@ -38,12 +46,12 @@ use Zend\View\Exception;
 
 /**
  * Extend url view helper to provide easy acces to subroutes of pages
- * 
- * The name of all page routes start with the id of the page. As this is an 
+ *
+ * The name of all page routes start with the id of the page. As this is an
  * unknown parameter for the view, this view helper prepends the root part
  * of the route. This happpens when the route name argument starts with a /.
- * 
- * Therefore common route names like "user" and "admin" will work, but for a 
+ *
+ * Therefore common route names like "user" and "admin" will work, but for a
  * special module Foo with a subroute "view-article", the route name /view-article
  * will be transformed to $id/view-article to match the appropriate page.
  *
@@ -72,13 +80,13 @@ class Url extends BaseUrl
             if ($this->routeMatch === null) {
                 throw new Exception\RuntimeException('No RouteMatch instance provided');
             }
-            
+
             $routeName = $this->routeMatch->getMatchedRouteName();
-            
+
             if ($routeName === null) {
                 throw new Exception\RuntimeException('RouteMatch does not contain a matched route name');
             }
-            
+
             if (false !== ($pos = strpos($routeName, '/'))) {
                 /**
                  * If we request the name /, it will mean that we need to only get
@@ -93,7 +101,7 @@ class Url extends BaseUrl
                 $name = $routeName . $name;
             }
         }
-        
+
         return parent::__invoke($name, $params, $options, $reuseMatchedParams);
     }
 }
